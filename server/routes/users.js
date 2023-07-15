62506c3084eb773230416b69d0bbe070ec4dc8fe
Param <kumarparam2000@gmail.com>
2023-07-15 17:23:10 +0530

    files add

---
+++ server/routes/users.js
@@ -0,0 +1,32 @@
+import  express  from "express";
+import { deleteUser, dislike, getUser, like, subscribe, unsubscribe, update } from '../controllers/user.js'
+import { verifyToken } from "../verifyToken.js";
+
+const router = express.Router();
+
+//update user
+router.put('/:id',verifyToken ,update)
+
+//delete user
+router.delete('/:id',verifyToken, deleteUser)
+
+//get user
+router.get('/find/:id', getUser)
+
+//subscribe user
+router.put('/sub/:id',verifyToken, subscribe)
+
+//unsubscribe user
+router.put('/unsub/:id',verifyToken, unsubscribe)
+
+//like user
+router.put('/like/:videoId',verifyToken, like)
+
+//dislike user
+router.put('/dislike/:videoId',verifyToken, dislike)
+
+
+
+
+
+export default router;
